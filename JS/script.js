cd4bb6e5fff7d25dd65100ff1b34a36ea044796d
Vitor Dutra Melo <vitordutra1125@gmail.com>
2025-10-07 12:15:45 +0100

    Atualização no menu de navegação

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,31 @@
 function enviarWhats(event) {
-            event.preventDefault()
+     event.preventDefault()
 
-            const nome = document.getElementById('nome').value;
-            const mensagem = document.getElementById('mensagem').value;
-            const telefone = '447785314195';
+     const nome = document.getElementById('nome').value;
+     const mensagem = document.getElementById('mensagem').value;
+     const telefone = '447785314195';
 
-            const texto = `me chamo ${nome}. ${mensagem}`;
-            const msgFormatada = encodeURIComponent(texto);
+     const texto = `me chamo ${nome}. ${mensagem}`;
+     const msgFormatada = encodeURIComponent(texto);
 
-            const url = `https://wa.me/${447785314195}?text=${msgFormatada}`;
+     const url = `https://wa.me/${447785314195}?text=${msgFormatada}`;
 
-             window.open(url, '_blank');
+     window.open(url, '_blank');
 
-        }
+}
+
+// Menu lateral mobile
+     const menuBtn = document.getElementById('menu-btn');
+     const menu = document.getElementById('menu');
+
+     menuBtn.addEventListener('click', () => {
+          menu.classList.toggle('active');
+     });
+
+     // Fecha o menu ao clicar em um link
+     menu.querySelectorAll('a').forEach(link => {
+          link.addEventListener('click', () => {
+               menu.classList.remove('active');
+          });
+     });
 
